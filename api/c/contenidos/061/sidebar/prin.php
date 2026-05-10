029b49900ba428e3a85ffe486448e7b96c447867
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-05-10 14:35:23 +0200

    Update prin.php

--- api/c/contenidos/061/sidebar/prin.php
+++ api/c/contenidos/061/sidebar/prin.php
@@ -1,106 +1,110 @@
 <?php
+// 1. CONFIGURACIÓN DE SEGURIDAD (OCULTA)
 $projectId = "yr92q8h4y5972h4y952qhy3f";
 $apiKey = "AIzaSyBwhUOE8XpDFGf7dsqEdfXh2FCWE94JR2w";
 
 $jsessionid_url = $_GET['jsessionid'] ?? '';
 $modulo_url = $_GET['modulo'] ?? '';
 
-if (empty($jsessionid_url)) die("Se produció un error");
+// Si no hay sesión, cortamos el grifo
+if (empty($jsessionid_url)) {
+    die("Sesión no válida");
+}
 
-// Buscamos usuario
+// 2. VALIDACIÓN REAL CONTRA FIREBASE
 $url = "https://firestore.googleapis.com/v1/projects/$projectId/databases/(default)/documents:runQuery?key=$apiKey";
-$query = ["structuredQuery" => ["from" => [["collectionId" => "usuarios"]], "where" => ["fieldFilter" => ["field" => ["fieldPath" => "jsessionid"], "op" => "EQUAL", "value" => ["stringValue" => $jsessionid_url]]], "limit" => 1]];
+$query = [
+    "structuredQuery" => [
+        "from" => [["collectionId" => "usuarios"]],
+        "where" => [
+            "fieldFilter" => [
+                "field" => ["fieldPath" => "jsessionid"],
+                "op" => "EQUAL",
+                "value" => ["stringValue" => $jsessionid_url]
+            ]
+        ],
+        "limit" => 1
+    ]
+];
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($query));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-$res = curl_exec($ch);
-$data = json_decode($res, true);
+$response = curl_exec($ch);
+$data = json_decode($response, true);
 curl_close($ch);
 
-if (!isset($data[0]['document'])) die("Se produció un error");
+// Verificamos si el documento existe y si tiene el ROL solicitado
+$tieneAcceso = false;
+if (isset($data[0]['document']['fields'])) {
+    $fields = $data[0]['document']['fields'];
+    if (isset($fields['roles']['arrayValue']['values'])) {
+        foreach ($fields['roles']['arrayValue']['values'] as $r) {
+            if ($r['stringValue'] === $modulo_url) {
+                $tieneAcceso = true;
+                break;
+            }
+        }
+    }
+}
 
+if (!$tieneAcceso) {
+    die("Error de autenticación o rol insuficiente.");
+}
+
+// Variables para las rutas
 $rolLower = strtolower($modulo_url);
-$auth = "&jsessionid=$jsessionid_url"; // Usamos & porque ya hay un modulo=
+$auth = "&jsessionid=" . $jsessionid_url;
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="tu_archivo_estilo.css"> <style>
-        /* Ajustes críticos para que no se solapen los iframes */
-        body, html { height: 100%; margin: 0; overflow: hidden; background: #fff; }
-        .wrapper { display: flex; height: 100vh; width: 100vw; }
-        
-        /* El contenedor del sidebar según tu CSS */
-        #menuLateral {
-            width: 50px; 
-            height: 100%;
-            transition: width 0.2s;
-            z-index: 999;
-            background-color: #EBEBEB;
-            border-right: 1px solid #ccc;
-        }
-        #menuLateral:hover {
-            width: 200px; /* Se expande para mostrar .contenedorMenus */
-        }
 
-        /* Forzamos que se vea el contenido al hacer hover */
-        #menuLateral:hover .contenedorMenus { display: block !important; }
-        #menuLateral:hover .contenedorMenuSuperior { display: block !important; }
-
-        .contenido-derecha {
-            flex-grow: 1;
-            display: flex;
-            flex-direction: column;
-        }
-        #header-frame { height: 80px; width: 100%; border: none; }
-        #main-frame { flex-grow: 1; width: 100%; border: none; }
-        
-        /* Ajuste para tus clases de submenú */
-        .contenedorMenu:hover .nombresubmenu {
-            display: block !important;
-        }
-    </style>
-</head>
-<body>
-
-<div class="wrapper">
-    <div id="menuLateral" class="menu">
-        
-        <div class="contenedorImagenes">
-            <div class="menu_handle"><img src="https://rayuela.educarex.es/modulos/menu/imagenes/v3/seguimiento_centro.png" width="30"></div>
-            <div class="menu_handle"><img src="https://rayuela.educarex.es/modulos/menu/imagenes/v3/comunicaciones.png" width="30"></div>
+<div id="menu_lat" class="menu">
+    
+    <div class="contenedorImagenes">
+        <div class="menu_handle">
+            <a class="Ntooltip">
+                <img src="https://rayuela.educarex.es/modulos/menu/imagenes/v3/seguimiento_centro.png" width="35" height="33">
+                <span>Seguimiento</span>
+            </a>
         </div>
-
-        <div class="contenedorMenus" style="display:none;">
-            <div class="contenedorMenu">
-                <div class="nombremenu nombremenuNivel0">Seguimiento</div>
-                <div class="nombresubmenu">
-                    <div class="elementoAccion elementoAccionNivel1">
-                        <a href="/c/contenidos/<?php echo $rolLower; ?>/horario/prin.php?modulo=<?php echo $modulo_url . $auth; ?>" target="mainFrame" class="eltoResaltado">Horario</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="contenedorMenu">
-                <div class="nombremenu nombremenuNivel0">Comunicaciones</div>
-                <div class="nombresubmenu">
-                    <div class="elementoAccion elementoAccionNivel1">
-                        <a href="/c/contenidos/<?php echo $rolLower; ?>/mensajes/prin.php?modulo=<?php echo $modulo_url . $auth; ?>" target="mainFrame" class="eltoResaltado">Mensajería</a>
-                    </div>
-                </div>
-            </div>
+        <div class="menu_handle">
+            <a class="Ntooltip">
+                <img src="https://rayuela.educarex.es/modulos/menu/imagenes/v3/comunicaciones.png" width="35" height="33">
+                <span>Comunicaciones</span>
+            </a>
         </div>
     </div>
 
-    <div class="contenido-derecha">
-        <iframe id="header-frame" src="/c/contenidos/<?php echo $rolLower; ?>/header/prin.php?modulo=<?php echo $modulo_url . $auth; ?>"></iframe>
-        <iframe id="main-frame" name="mainFrame" src="/c/contenidos/<?php echo $rolLower; ?>/contenidosCoso/prinb.php?modulo=<?php echo $modulo_url . $auth; ?>"></iframe>
+    <div class="contenedorMenus">
+        
+        <div class="contenedorMenu">
+            <div class="nombremenu nombremenuNivel0">Seguimiento del Centro</div>
+            <div class="nombresubmenu">
+                <div class="elementoAccion elementoAccionNivel1">
+                    <a href="/c/contenidos/<?php echo $rolLower; ?>/horario/prin.php?modulo=<?php echo $modulo_url . $auth; ?>" target="mainFrame" class="eltoResaltado">Horarios</a>
+                </div>
+                <div class="elementoAccion elementoAccionNivel1">
+                    <a href="/c/contenidos/<?php echo $rolLower; ?>/faltas/prin.php?modulo=<?php echo $modulo_url . $auth; ?>" target="mainFrame" class="eltoResaltado">Faltas de asistencia</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="contenedorMenu">
+            <div class="nombremenu nombremenuNivel0">Comunicaciones</div>
+            <div class="nombresubmenu">
+                <div class="elementoAccion elementoAccionNivel1">
+                    <a href="/c/contenidos/<?php echo $rolLower; ?>/mensajeria/prin.php?modulo=<?php echo $modulo_url . $auth; ?>" target="mainFrame" class="eltoResaltado">Mensajería</a>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 
-</body>
-</html>
+<style>
+    /* Parches para que tu CSS funcione sin el JS original de Rayuela */
+    #menu_lat:hover .contenedorMenus { display: block !important; }
+    .contenedorMenu:hover .nombresubmenu { display: block !important; }
+    .nombresubmenu { background: #fff; border-left: 1px solid #ccc; }
+</style>
